a91344cf06075baaddfc08111c86a818b3c4e363
Neven Bedr <neven2225@gmail.com>
2020-07-09 17:40:54 +0000

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(){
+ 
+  const drivers = ['Sally','Bob', 'Freddy', 'Cladia']
+  return [
+      drivers[0],
+      drivers[1]
+    ]
+}
+
+
+function returnLastTwoDrivers() {
+  const drivers = ['Sally','Bob', 'Freddy', 'Claudia']
+  return [drivers[2], drivers[3]]
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(){
+    return fareQuintupler
+}
+
+function fareQuintupler(num) {
+  return num * num
+}
+
+function fareDoubler(fare){
+  return fare * 2;
+
+}
+
+function fareTripler(fare){
+  return fare * 3;
+
+}
+
+
+function selectDifferentDrivers(drivers, func){
+  return func(drivers)
+}
